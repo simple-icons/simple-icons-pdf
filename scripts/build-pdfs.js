6cf666c1ae80a5cb27afb9faf86114de7445b827
Adam Rusted <hello@adamrusted.me>
2021-05-29 21:26:37 +0100

    Update build-pdfs.js

--- scripts/build-pdfs.js
+++ scripts/build-pdfs.js
@@ -8,12 +8,11 @@ const { getIconSlug } = require('./utils.js');
 
 for (const title in simpleIcons) {
   const icon = simpleIcons.Get(title);
-  const iconSlug = getIconSlug(icon);
   let doc = new PDFDocument({
     size: [24, 24],
     margins: { top: 0, bottom: 0, left: 0, right: 0 },
   });
-  let stream = fs.createWriteStream(`./icons/${iconSlug}.pdf`);
+  let stream = fs.createWriteStream(`./icons/${icon.slug}.pdf`);
   let svg = icon.svg;
   SVGtoPDF(doc, svg, 0, 0);
   doc.pipe(stream);
