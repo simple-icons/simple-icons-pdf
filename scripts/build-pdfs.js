f030a02063ddb0089181bfc2744ba4d47694b0fc
Adam Rusted <hello@adamrusted.me>
2021-05-30 12:41:37 +0100

    Optimize Script

--- scripts/build-pdfs.js
+++ scripts/build-pdfs.js
@@ -5,13 +5,12 @@ const SVGtoPDF = require('svg-to-pdfkit');
 const simpleIcons = require('simple-icons');
 
 for (const slug in simpleIcons) {
-  const icon = simpleIcons.Get(slug);
+  const { svg } = simpleIcons.Get(slug);
   let doc = new PDFDocument({
     size: [24, 24],
     margins: { top: 0, bottom: 0, left: 0, right: 0 },
   });
   let stream = fs.createWriteStream(`./icons/${slug}.pdf`);
-  let svg = icon.svg;
   SVGtoPDF(doc, svg, 0, 0);
   doc.pipe(stream);
   doc.end();
