83b8f7425de01a52478c8f39daa9628de15f821a
Adam Rusted <hello@adamrusted.me>
2021-05-30 12:40:31 +0100

    Update build-pdfs.js

--- scripts/build-pdfs.js
+++ scripts/build-pdfs.js
@@ -4,13 +4,13 @@ const PDFDocument = require('pdfkit');
 const SVGtoPDF = require('svg-to-pdfkit');
 const simpleIcons = require('simple-icons');
 
-for (const title in simpleIcons) {
-  const icon = simpleIcons.Get(title);
+for (const slug in simpleIcons) {
+  const icon = simpleIcons.Get(slug);
   let doc = new PDFDocument({
     size: [24, 24],
     margins: { top: 0, bottom: 0, left: 0, right: 0 },
   });
-  let stream = fs.createWriteStream(`./icons/${icon.slug}.pdf`);
+  let stream = fs.createWriteStream(`./icons/${slug}.pdf`);
   let svg = icon.svg;
   SVGtoPDF(doc, svg, 0, 0);
   doc.pipe(stream);
