77931e00403e1c3ae4e79896bf83eca52e9f1a32
Eric Cornelissen <ericornelissen@gmail.com>
2021-06-27 13:54:51 +0200

    Remove creation date from generated PDFs (#4)
    * Remove creation date from genrated PDFs
    
    The creation date included in the generated PDFs causes `npm run build`
    to update all PDFs with a new creation date, causing a lot of
    unnecessary changes. This hacks the CreateDate value to be an empty
    string, preventing regeneration of the PDFs from causing changes to all
    PDFs.
    
    * Update all PDFs

--- scripts/build-pdfs.js
+++ scripts/build-pdfs.js
@@ -5,12 +5,20 @@ const PDFDocument = require('pdfkit');
 const SVGtoPDF = require('svg-to-pdfkit');
 const simpleIcons = require('simple-icons');
 
+// Create a fake CreationDate for PDFs that results in an empty string. This
+// prevents changes to the PDF if the corresponding SVG didn't change.
+const CreationData = new String('');
+CreationData.getTime = () => '';
+
 for (const title in simpleIcons) {
   const icon = simpleIcons.get(title);
   const iconSlug = icon.slug || titleToSlug(icon.title);
   let doc = new PDFDocument({
     size: [24, 24],
     margins: { top: 0, bottom: 0, left: 0, right: 0 },
+    info: {
+      CreationDate: CreationData,
+    },
   });
   let stream = fs.createWriteStream(`./icons/${iconSlug}.pdf`);
   let svg = icon.svg;
