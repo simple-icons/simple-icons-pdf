26e1c8e5af0cf19ca3b0ec4dbd01122cffe16c11
Adam Rusted <hello@adamrusted.me>
2021-06-27 13:10:55 +0100

    Update syntax to v5 (#3)
    * Update syntax to v5 - resolve some minor errors.
    
    * Add .gitattributes
    
    * Pin simple-icons to latest version
    
    * Rebuild PDFs
    
    Co-authored-by: Eric Cornelissen <ericornelissen@gmail.com>

--- scripts/build-pdfs.js
+++ scripts/build-pdfs.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 
 const PDFDocument = require('pdfkit');
 const SVGtoPDF = require('svg-to-pdfkit');
@@ -10,9 +9,8 @@ const simpleIcons = require('simple-icons');
 const CreationData = new String('');
 CreationData.getTime = () => '';
 
-for (const title in simpleIcons) {
-  const icon = simpleIcons.get(title);
-  const iconSlug = icon.slug || titleToSlug(icon.title);
+for (const slug in simpleIcons) {
+  const { svg } = simpleIcons.Get(slug);
   let doc = new PDFDocument({
     size: [24, 24],
     margins: { top: 0, bottom: 0, left: 0, right: 0 },
@@ -20,8 +18,7 @@ for (const title in simpleIcons) {
       CreationDate: CreationData,
     },
   });
-  let stream = fs.createWriteStream(`./icons/${iconSlug}.pdf`);
-  let svg = icon.svg;
+  let stream = fs.createWriteStream(`./icons/${slug}.pdf`);
   SVGtoPDF(doc, svg, 0, 0);
   doc.pipe(stream);
   doc.end();
